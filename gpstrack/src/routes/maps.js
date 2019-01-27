2a072d1644aa76274a09a27d28ed42f9a0340691
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2019-01-27 11:15:44 -0500

    Prototype

--- gpstrack/src/routes/maps.js
+++ gpstrack/src/routes/maps.js
@@ -13,12 +13,28 @@ router.get('/record', isAuthenticated, async (req, res) => {
     res.render('maps/record');
 });
 
+//Save Route 
 router.post('/newroute', isAuthenticated, async(req, res) => {
     const newRoute = new Route({user: req.user.id, 
         lat: JSON.parse(req.body.latArr), lon: JSON.parse(req.body.lonArr)});
     await newRoute.save();
     req.flash('success_msg', 'Route Added Successfully');
     res.redirect('/');
+});
+
+//Show all routes
+router.get('/routes', isAuthenticated, async(req, res) => {
+    const routes = await Route.find({
+        user: req.user.id}).sort({date: 'desc'});
+        console.log(routes);
+    res.render('maps/all-routes', {routes});
+});
+
+//Delete
+router.delete('/routes/delete/:id', isAuthenticated, async(req, res) => {
+    await Route.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', "Route deleted successfully");
+    res.redirect('/routes');
 });
 
 module.exports = router;
