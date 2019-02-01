a269787cc86e19b9c19cdfaf3bfd6fd92e031f67
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2019-02-01 13:05:45 -0500

    Setting up GoogleMaps

--- gpstrack/src/public/js/map.js
+++ gpstrack/src/public/js/map.js
@@ -1,10 +1,3 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-
-//Solo tengo que enviar el formulario de routeform con AJAX
-
 var map, infoWindow, watchID, routeId, socket;
 var user = document.getElementById('user').value;
 
@@ -13,19 +6,17 @@ function initMap() {
     var socket = io();
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 6
+        zoom: 16
     });
     infoWindow = new google.maps.InfoWindow;
 }
 
 function record() {
-    //Antes que nada debemos crear la Ruta en la base de datos
-    //y obtener su Id para poder asociar los puntos que van a ser recogidos
-    //document.forms["routeForm"].submit(); //Como recupero la respuesta de aqui para obtener el routeId ??? 
+    //Create the route in teh database
+    //and associate that route with the points
     document.getElementById("recordbtn").disabled = true;
-
-    var socket = io();
     
+    var socket = io();
     socket.emit('new route', user);
     socket.on('new route', function (route) {
         routeId = JSON.parse(route);
@@ -39,25 +30,13 @@ function record() {
                 lng: position.coords.longitude
             };
 
-            //Aqui mostraremos el punto en el mapa
-            var h = document.createElement("H5");
-            h.style.color = '#FFFFFF'
-            var text = document.createTextNode("Latitud" + pos.lat + "Longitud : " + pos.lng);
-            document.getElementById('container').appendChild(h.appendChild(text));
-
-            //Aqui guardamos el punto en la base de datos 
-            document.getElementById('lat').value = pos.lat;
-            document.getElementById('lon').value = pos.lng;
-            document.getElementById('routeId').value = routeId;
-            //document.forms["pointForm"].submit(); //Como recarga la pagina, es mejor usar AJAX
-            //document.getElementById('pointForm').submit(); //Necesita ser añadido con sockets
+            //Save the point to database
             socket.emit('new point', { lat: pos.lat, lon: pos.lng, routeId: routeId });
-            //console.log(pos.lat, pos.lng);
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
+            //Show the point in the map
+            var marker = new google.maps.Marker({ position: pos });
+            marker.setMap(map);
+            map.setCenter(marker.getPosition());
         }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
         });
